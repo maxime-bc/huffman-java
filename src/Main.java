71ad9cbc77ce881714d21c5c04226b1882d0cdc0
maxime-bc <maximeblanchon91490@gmail.com>
2020-03-17 23:56:05 +0100

    Command Line Parser (file and string input)

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,44 @@
+import picocli.CommandLine;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        String myString = "Hello world !";
-        StringOccurrencesCounter stringOccurrencesCounter = new StringOccurrencesCounter(myString);
+        CommandLineParser commandLineParser = new CommandLineParser();
+        picocli.CommandLine cmd = new picocli.CommandLine(commandLineParser);
 
+        try {
+            cmd.parseArgs(args);
+        } catch (CommandLine.MutuallyExclusiveArgsException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        String inputFile = commandLineParser.exclusive.inputFile;
+        String inputString = commandLineParser.exclusive.inputString;
+
+        if (inputFile != null) {
+            // An input file was provided, so we store its content into `inputString`
+
+            Path path = Paths.get(inputFile);
+
+            if (Files.exists(path) && Files.isRegularFile(path)) {
+                try {
+                    inputString = Files.readString(path);
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            } else {
+                System.out.println(inputFile + " does not exists.");
+                System.exit(1);
+            }
+        }
+        StringOccurrencesCounter stringOccurrencesCounter = new StringOccurrencesCounter(inputString);
         String res = stringOccurrencesCounter.getOccurrences();
         System.out.println(res);
-
     }
 }
